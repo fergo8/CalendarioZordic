65750705cb614ac7f7a5f5923778c5d47ed56c80
Jeff <30440307+fergo8@users.noreply.github.com>
2022-06-24 08:37:10 -0300

    teste criar título da página

--- Calendario/app.js
+++ Calendario/app.js
@@ -2,5 +2,8 @@ document.addEventListener("DOMContentLoaded", function(e) {
     alert("hello world!")
     var app = document.getElementById("app");
 
-    app.appendChild("<p>Olá mundo!</p>")
+    const titulo = document.createElement("h1")
+    titulo.appendChild(document.createTextNode("Olá mundo!"))
+
+    app.appendChild(titulo)
 })
