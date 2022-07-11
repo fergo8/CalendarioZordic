d27445d3f8322a0625011a6e80f113ed2cd01576
Jeff <30440307+fergo8@users.noreply.github.com>
2022-07-11 01:11:20 -0300

    Ajuste grid calendário

--- Calendario/Controller/App.js
+++ Calendario/Controller/App.js
@@ -13,21 +13,48 @@ document.addEventListener("DOMContentLoaded", function(e) {
 
     // Criar calendário
     const articleCalendario = document.createElement("article")
-    articleCalendario.setAttribute("class", "row")
 
+    const divLinha1 = document.createElement("div")
+    const divLinha2 = document.createElement("div")
+    const divLinha3 = document.createElement("div")
+    divLinha1.setAttribute("class", "row")
+    divLinha2.setAttribute("class", "row")
+    divLinha3.setAttribute("class", "row")
+
+    for (var mes = 0; mes < calendario.diasDoMes.length; mes++) {
+        if (calendario.diasDoMes[mes].posicaoCalendario == 1) {
+            divLinha1.appendChild(CriarElementoHTML(
+                "div",
+                { "class": "col" },
+                calendario.diasDoMes[mes].dia
+            ))
+        }
+        if (calendario.diasDoMes[mes].posicaoCalendario == 2) {
+            divLinha2.appendChild(CriarElementoHTML(
+                "div",
+                { "class": "col" },
+                calendario.diasDoMes[mes].dia
+            ))
+        }
+        if (calendario.diasDoMes[mes].posicaoCalendario == 3) {
+            divLinha3.appendChild(CriarElementoHTML(
+                "div",
+                { "class": "col" },
+                calendario.diasDoMes[mes].dia
+            ))
+        }
+    }
+
+    articleCalendario.appendChild(divLinha1)
+    articleCalendario.appendChild(divLinha2)
+    articleCalendario.appendChild(divLinha3)
+
+    // Teste data atual
     articleCalendario.appendChild(CriarElementoHTML(
         "p",
         { "class": "diaDeHoje" },
         calendario.hoje.dia + "/" + calendario.hoje.mes + "/" + calendario.hoje.ano
     ))
-
-    for (var mes = 0; mes < calendario.diasDoMes.length; mes++) {
-        articleCalendario.appendChild(CriarElementoHTML(
-            "div",
-            { "class": "col" },
-            calendario.diasDoMes[mes].dia
-        ))
-    }
 
     // Montar página
     app.appendChild(articleMenu)
