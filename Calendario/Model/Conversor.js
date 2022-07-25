298f7a63a1e35c633ed40ee9eee7c8d5539c5ec7
Jeff <30440307+fergo8@users.noreply.github.com>
2022-07-25 20:57:54 -0300

    Estilo do conversor

--- Calendario/Model/Conversor.js
+++ Calendario/Model/Conversor.js
@@ -12,18 +12,23 @@ class Conversor {
 
         const labelGregoriano = document.createElement("label")
         labelGregoriano.setAttribute("class", "conversor-label")
+        labelGregoriano.innerText = "Gregoriano"
         
         const labelCalendario = document.createElement("label")
         labelCalendario.setAttribute("class", "conversor-label")
+        labelCalendario.innerText = "Calendário novo"
         
         const inputGregoriano = document.createElement("input")
         inputGregoriano.setAttribute("class", "conversor-input")
+        inputGregoriano.setAttribute("id", "data-gregoriano")
         
         const inputCalendario = document.createElement("input")
         inputCalendario.setAttribute("class", "conversor-input")
+        inputCalendario.setAttribute("id", "data-calendario-novo")
         
         const submit = document.createElement("buttom")
         submit.setAttribute("class", "conversor-submit")
+        submit.setAttribute("id", "btm-converter")
         submit.setAttribute("type", "submit")
 
         formConversor.appendChild(labelGregoriano)
